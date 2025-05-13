2bf2c37a7fe802d4fe0c65eb7f54643d6d44ab5e
harshakl03 <harshaharsha87593@gmail.com>
2025-05-13 23:38:07 +0530

    Reusable components added

--- FrontEnd/src/ui/Footer.jsx
+++ FrontEnd/src/ui/Footer.jsx
@@ -1,8 +1,7 @@
 export default function Footer() {
   return (
-  <footer class="bg-blue-600 text-white text-center p-3 font-medium text-lg">
+    <footer class="bg-blue-600 text-white text-center p-3 font-medium text-lg">
       “Learn everyday so that you can rule the world everyday”
-  </footer>
-    
+    </footer>
   );
 }

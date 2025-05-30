f64f1650011f783b943050da72d57f7349741f38
harshakl03 <harshaharsha87593@gmail.com>
2025-05-30 23:36:44 +0530

    Data loading fixed in dashboard for recent documents

--- FrontEnd/src/features/User/useUploadDocument.jsx
+++ FrontEnd/src/features/User/useUploadDocument.jsx
@@ -1,12 +1,14 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { apiUploadDocument } from "../../utils/apiUser";
 import toast from "react-hot-toast";
 
 export default function useUploadDocument() {
+  const query = useQueryClient();
   const { mutate: uploadDocument, isPending: isLoading } = useMutation({
     mutationFn: (data) => apiUploadDocument(data),
     onSuccess: (data) => {
       toast.success(data.message);
+      query.invalidateQueries(["data"]);
     },
     onError: (error) => {
       toast.error(error.message);
